243f23b4bf9fa10807ab5ec030ab893484b556a6
Anjali Singhal <singhalanjali0429@gmail.com>
2024-08-05 23:12:56 +0530

    adding HomeTimeTable screen

--- app/src/main/java/com/example/classmanagementsystem/screens/HomeTimeTable.kt
+++ app/src/main/java/com/example/classmanagementsystem/screens/HomeTimeTable.kt
@@ -1,19 +1,51 @@
 package com.example.classmanagementsystem.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.classmanagementsystem.R
+import com.example.classmanagementsystem.components.FileTopBar
+import com.example.classmanagementsystem.navigation.AppRouter
+import com.example.classmanagementsystem.navigation.Screen
+import com.example.classmanagementsystem.navigation.SystemBackButtonHandler
 
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
 fun HomeTimetable() {
-    // Your UI components for the Timetable screen
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "This is the Timetable Screen")
+    Scaffold(
+        topBar = {
+            FileTopBar(
+                toolbarTitle = stringResource(id = R.string.TimeTable),
+                navigationIconClicked = {
+                    AppRouter.navigateTo(Screen.HomeScreen)
+                }
+            )
+        }
+    ){innerPadding->
+        Column(
+            modifier= Modifier
+                .padding(16.dp)
+                .fillMaxSize()
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState()),
+//                .align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ){
+
+        }
+        SystemBackButtonHandler {
+            AppRouter.navigateTo(Screen.HomeScreen)
+        }
     }
 }
